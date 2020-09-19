010ccadc9a601a3436f7455bcd515ab50d00ea08
Lenny LD <lenmorld@gmail.com>
2020-09-19 18:55:44 -0400

    test route

--- express/server.js
+++ express/server.js
@@ -5,7 +5,7 @@ const app = express();
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
 
-const router = express.Router();
+// const router = express.Router();
 
 const studify = require('./studify')
 const react_workshop = require('./react_workshop')
@@ -18,12 +18,13 @@ const react_workshop = require('./react_workshop')
 // router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 // use react_workshop routes
-router.use("/", react_workshop);
+// router.use("/", react_workshop);
 
 // use studify routes
 // router.use("/", studify);
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', studify);  // path must route to lambda
+app.use('/.netlify/functions/server/react', react_workshop);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
